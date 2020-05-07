3c5cb59ed1e3cebd247d4fc68a0373892499467f
Josef Fürlinger <josef.fuerlinger@gmail.com>
2020-05-07 18:37:18 +0200

    Implement OrderCoffeeThruListModel

---
+++ source/CoffeeSlotMachine.Web/Pages/OrderCoffeeThruList.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CoffeeSlotMachine.Core;
+using CoffeeSlotMachine.Core.Contracts;
+using CoffeeSlotMachine.Core.DataTransferObjects;
+using CoffeeSlotMachine.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoffeeSlotMachine.Web.Pages
+{
+  public class OrderCoffeeThruListModel : PageModel
+  {
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ProductDto[] OrderCoffeeItems { get; set; }
+
+    public OrderCoffeeThruListModel(IUnitOfWork unitOfWork)
+    {
+      _unitOfWork = unitOfWork;
+    }
+
+    public async Task OnGet()
+    {
+      ViewData["Message"] = "Welcome to our coffee machine!";
+      OrderCoffeeItems = await _unitOfWork.Products.GetProductDtosAsync();
+    }
+
+    public async Task<IActionResult> OnPostProductSelected(int productId)
+    {
+      if (!ModelState.IsValid)
+      {
+        return Page();
+      }
+
+      var orderController = new OrderController(_unitOfWork);
+
+      Product product = await _unitOfWork.Products.GetByIdAsync(productId);
+      Order order = await orderController.OrderCoffeeAsync(product);
+
+      return RedirectToPage(
+          "/InsertCoin",
+          new { orderId = order.Id }
+          );
+    }
+  }
+}
